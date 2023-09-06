ea74a8375353df31ddfb8d62a9be559daeea8873
favvy04 <funmilolaadebiyi52@gmail.com>
2023-09-06 16:00:17 +0100

    commit changes

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const btn = document.querySelector(".secondary-button");
-// const notificationBarge = document.querySelector(".notification-barge");
-const article = document.querySelector(".notification");
+const unreadNotification = document.querySelectorAll(".unreadNotification");
 const displayMessage = document.querySelector(".display-message");
 const privateMessage = document.querySelector(".private-message");
 const notificationBtn= document.querySelector(".notification-btn");
-btn.addEventListener('click',function(){
- article.classList.add("mark-all-as-read");
-//  notificationBarge.classList.add("mark-all-as-read");
+const notificationBarge = document.querySelectorAll(".notification-barge");
+const notification = document.querySelectorAll(".notification");
+unreadNotification.forEach(function(unreadNotification){
+    btn.addEventListener('click',function(){
+        unreadNotification.classList.add("read-notification");
+       });
 });
+
 displayMessage.addEventListener('click',function(){
     privateMessage.classList.toggle('show-message');
 });
-// 
+notification.forEach(function(notification){
+    notificationBtn.addEventListener('click',function(){
+        if(notification.classList.contains("notification-barge")){
+            notification.style.display="block";
+        }else{
+            notification.style.display = "none";
+        }
+        
+    });
+});
